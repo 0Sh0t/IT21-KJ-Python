8a189d669e79f97893fff1834314f71e414b2f39
KevJn <94998639+0Sh0t@users.noreply.github.com>
2022-01-31 13:34:37 +0200

    Add files via upload
    Ülesanne 01

---
+++ Python3 ül1.py
@@ -0,0 +1,23 @@
+# Kevin Joarand
+# 31.01.2022
+# Ülesanne 01
+
+a = int(input("Sisesta üks külg a="))
+b = int(input("Sisesta teine külg b="))
+s = a * b
+print ("ristküliku pindala:",str(s)+"cm^2")
+
+
+
+
+# print käsk väljastab teksti
+print("Cabernet Sauvignon\" või 'Cabernet Sauvignon'?")
+
+print ("  @..@ ")
+print (" (----)")
+print ("( \__/ )")
+print ("^^ \"\" ^^")
+
+int(input("ruudu külje pikkus: a="))
+S = a * a
+print ("ruudu pindala on ",str(S)+"cm^2")
