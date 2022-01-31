bc009eb484d2b18102b304f14a03a64d05689d95
KevJn <94998639+0Sh0t@users.noreply.github.com>
2022-01-31 14:02:15 +0200

    Add files via upload

---
+++ Python3 ül2.py
@@ -0,0 +1,31 @@
+# Kevin Joarand
+# 31.01.2022
+# Ülesanne 02
+
+print("Kolmurga ümbermõõt")
+#kolmnurga ümbermõõõõõõt
+a,b,c = 1,2,3
+p= a+b+c
+print("Kolmnurga ümbermõõt on:",p)
+
+print("Toote hind")
+#toote hinna leidmineeeee
+hind = 36.75
+ale = 0.4
+kogus = 3
+summa = (hind-(hind*ale))*3
+print("Kolme toote summa:",summa, "€")
+
+print("Pitsa")
+#pitsa eest maksmine
+hind = 12.90
+jootraha = 0.1
+summa = hind * jootraha
+print("Igaüks peab maksma:",summa,"€")
+
+Print("Rulluisutajad")
+#keskmine kiirus
+kiirus = 29.9
+aeg = 24
+kaugus = kiirus * aeg
+print("24 Min jõuab:",kaugus,) 
