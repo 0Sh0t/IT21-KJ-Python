ce47acc7c305c914337208065a331073caefb900
KevJn <94998639+0Sh0t@users.noreply.github.com>
2022-02-01 15:07:14 +0200

    Add files via upload
    Ülesanne 02

--- Python3 ül2.py
+++ Python3 ül2.py
@@ -1,6 +1,9 @@
 # Kevin Joarand
 # 31.01.2022
 # Ülesanne 02
+import math
+
+
 
 print("Kolmurga ümbermõõt")
 #kolmnurga ümbermõõõõõõt
@@ -8,6 +11,7 @@ a,b,c = 1,2,3
 p= a+b+c
 print("Kolmnurga ümbermõõt on:",p)
 
+print("----------------------")
 print("Toote hind")
 #toote hinna leidmineeeee
 hind = 36.75
@@ -16,6 +20,7 @@ kogus = 3
 summa = (hind-(hind*ale))*3
 print("Kolme toote summa:",summa, "€")
 
+print("----------------------")
 print("Pitsa")
 #pitsa eest maksmine
 hind = 12.90
@@ -23,9 +28,38 @@ jootraha = 0.1
 summa = hind * jootraha
 print("Igaüks peab maksma:",summa,"€")
 
-Print("Rulluisutajad")
+print("----------------------")
 #keskmine kiirus
 kiirus = 29.9
 aeg = 24
 kaugus = kiirus * aeg
-print("24 Min jõuab:",kaugus,) 
+print("24 Min jõuab:",kaugus,)
+
+print("----------------------")
+#Leia kolmnurga hüpotenuus
+a = 16
+b = 9
+c = math.sqrt(a**2+b**2)
+print("Kolmnurga hüpotenuus on:",c, "cm2")
+
+print("----------------------")
+#Ajateisendus
+minutid = int(input("Sisesta minutid:"))
+sisestus = minutid//60
+vastus = minutid % 60
+print("Tunde on:",sisestus )
+
+print("----------------------")
+#Arvusüsteemid
+arv = int(input("Sisesta 10nd arv: "))
+print(bin(arv))
+print(hex(arv))
+
+#Kütusekulu arvutamine
+print("----------------------")
+kutus = int(input("Sisesta kütuse liitrid:"))
+kilomeetrid = int(input("Sisesta läbitud kilomeetrid:"))
+keskmine = 100
+kutusekulu = kutus/(kilomeetrid*100)
+print(kutusekulu)
+
