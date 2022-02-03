7aff416a1196193e4223a6a1e01723518fb3cf42
KevJn <94998639+0Sh0t@users.noreply.github.com>
2022-02-03 11:37:28 +0200

    Add files via upload
    Ülesanne 04

---
+++ Python3 ül4.py
@@ -0,0 +1,59 @@
+'''
+Kevin Joarand
+03.02.2022
+Ülesanne 04
+'''
+
+
+
+
+
+
+#Juubel Valesti
+spaev = int(input("Sisesta oma synna"))
+j1 = 10,20,30,40,50,60,70,80,90,100
+if spaev =="j1":
+    print("!!Juubel!!: ")
+else:
+    print("Ei ole juubel: ")
+
+#Juubel
+synnipaev = int(input("Sisesta oma sünnipäev dd.mm.yyyy: "))
+a.b.c = synnipaev.split(".")
+aasta = 2022
+vanus = aasta - c
+jaak = vanus % 5
+
+print("a.b.c")
+
+if synnipäev==
+elif
+
+#Matemaatika
+
+a = int(input("Siesta arv1: "))
+b = int(input("Sisesta arv2: "))
+tehe = input("Vali tehe:\n + Liitmine \n - Lahutamine \n * Korrutamine \n : Jagamine \n")
+
+
+if tehe=="+":
+    v = a + b
+elif tehe=="-":
+    v = a - b
+elif tehe=="*":
+    v = a * b
+elif tehe==":":
+    v = a / b
+
+else:
+    vastus
+print(f"{a}{tehe}{b}={v}")
+
+#Ruut
+B = int(input("1. külg: "))
+D = int(input("2. külg: "))
+
+if B==D:
+        print("Ruut")
+else:
+        print("Ristküik")
