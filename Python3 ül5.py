e7a6faa778bdfc095b7abf757bc4d7a679d732c3
KevJn <94998639+0Sh0t@users.noreply.github.com>
2022-02-10 13:59:47 +0200

    Add files via upload
    Ülesanne 05

---
+++ Python3 ül5.py
@@ -0,0 +1,55 @@
+'''
+Kevin Joarand
+10.02.2022
+Ülesanne 05
+'''
+
+#Tärnid
+
+
+
+#Vanused
+
+import random
+
+arvud = []
+for i in range(5):
+    arvud.append(random.randint(1,99))
+print(f"suurim arv: {max(arvud)}\n väiksem arv: {min(arvud)}\n summa: {sum(arvud)/len(arvud)} ")    
+#for i in range(len(arvud)):
+#    print(arvud)
+
+#Duplikaadid
+
+kordused = ['Juhan','Kati','Mario','Mario','Mati','Mati']
+puhastatud = []
+
+for i in range(len(kordused)):
+    if kordused[i] not in puhastatud:
+        puhastatud.append(kordused[i])
+
+for i in range(len(puhastatud)):
+    print(puhastatud[i],end=", ")
+
+#Õpilased
+
+opilased = ['Juhan','Kati','Maarja','Mario','Mati']
+for i in range(len(opilased)):
+    print (f"{i+1}. {opilased[i]}")
+valik = int(input("Sisesta number: "))
+del opilased[valik-1]
+
+uus_nimi = input("Sisesta uus või muudetud nimi: ")
+opilased.insert(valik-1,uus_nimi)
+
+print("Nimi muudetud")
+print(opilased)
+
+#Nimede lisamine loendisse
+nimed = []
+for i in range(5):
+    nimi = input("Lisa nimi: ")
+    nimed.append(nimi)
+nimed.sort()
+print(nimed)
+    
