06513aa366a6db5cdae0360d81cec81dda937ac3
KevJn <94998639+0Sh0t@users.noreply.github.com>
2022-03-08 15:47:06 +0200

    Add files via upload

---
+++ Kodutöö 1.py
@@ -0,0 +1,19 @@
+'''
+Kevin Joarand
+Kodutöö 1
+08.03.2022
+'''
+
+#Pilved
+kysimus = int(input("Sisesta pilvede aluse kõrgus (kilomeetrites): "))
+if kysimus = 1,2,3,4,5,6:
+    print("Need ei ole ülemised pilved")
+else:
+    print("Need on ülemised pilved")
+
+# Aasta liblikas
+aasta = 2020
+liblikas = "teelehe-mosaiikliblikas"
+lausekeskosa = print(aasta"liblikas on: "liblikas)
+lause = print(aasta,lause keskosa,liblikas)
+print(lause)
