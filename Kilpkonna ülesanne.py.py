bd6d1cda674fdf3cb44c255dc5e22c2a0c096111
KevJn <94998639+0Sh0t@users.noreply.github.com>
2022-03-15 00:34:56 +0200

    Add files via upload

---
+++ Kilpkonna ülesanne.py.py
@@ -0,0 +1,32 @@
+'''
+Kevin Joarand
+Kilpkonn
+14.03.2022
+'''
+
+import turtle
+
+#Akna seaded
+aken = turtle.Screen()
+aken.setup(500,500)
+aken.title("Ülesanne")
+
+#Funktsioon ruudu tekitamiseks
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.left(60)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+turtle.exitonclick()
