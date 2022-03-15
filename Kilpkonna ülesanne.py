b8e9617263714b580e1e58e619ef874d51051cf5
KevJn <94998639+0Sh0t@users.noreply.github.com>
2022-03-15 13:36:09 +0200

    Add files via upload

--- Kilpkonna ülesanne.py
+++ Kilpkonna ülesanne.py
@@ -10,23 +10,25 @@ import turtle
 aken = turtle.Screen()
 aken.setup(500,500)
 aken.title("Ülesanne")
-
+nurk = 0
 #Funktsioon ruudu tekitamiseks
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
-turtle.left(90)
-turtle.left(60)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
+for i in range(2):
+    t=turtle.Turtle()
+    t.right(nurk)
+    t.penup()
+    t.forward(50)
+    t.pendown()
+    t.right(90)
+    t.forward(50)
+    t.right(90)
+    t.forward(100)
+    t.right(90)
+    t.forward(100)
+    t.right(90)
+    t.forward(100)
+    t.right(90)
+    t.forward(50)
+    nurk = 45
+    
 
 turtle.exitonclick()
