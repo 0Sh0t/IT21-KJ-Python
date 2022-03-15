c45b8f16fc7505d39e25210809773575b1f0c0e7
KevJn <94998639+0Sh0t@users.noreply.github.com>
2022-03-15 15:29:38 +0200

    Add files via upload

--- Kontrolltöö ül.3.py
+++ Kontrolltöö ül.3.py
@@ -1,95 +1,54 @@
 '''
 Kevin Joarand
 Kontrolltöö ül.3
-03.14.2022
+15.03.2022
 '''
 
 import random
+game = 1
+while game == 1:
+    oiged = 0
+    korrad = int(input("Sisesta kordade arv"))
+    
+#Valitakse suvalised arvud
+    for i in range(korrad):
+        arv1 = random.randint(1, 10)
+        arv2 = random.randint(1, 10)
+#Tehakse liitmis ja lahutamis tehted
+        tehe = random.randint(1, 2)
+        if tehe == 1:
+            loop = 1
+            while loop == 1:
+                vastus = arv1 + arv2
+                v = int(input(f"{arv1} + {arv2}="))
+                if v == vastus:
+                    print("Õige")
+                    oiged += 1
+                    loop = 0
+                else:
+                    print("Vale")
+                
+        else:
+            loop = 1
+            while loop == 1:
+                vastus = arv1 - arv2
+                v = int(input(f"{arv1} - {arv2}="))
+                if v == vastus:
+                    print("Õige")
+                    oiged += 1
+                    loop = 0
+                else:
+                    print("Vale")
 
-#Funktsioon kuvab algus teksti
-def algus():
-  tekst = 'Liitmine ja lahutamine'
-  print('*' * len(tekst))
-  print(tekst)
-  print('*' * len(tekst))
-  
-#Funktsioon annab valida liitmis ja lahutamis tehte vahel
-def tehted():
-  valik = ["1. Liitmine" , "2. Lahutamine" , "3. EXIT"]
-  print(valik[0])
-  print(valik[1])
-
-  
-#Funktsioon vastab kasutaja sisestusele
-def xsisestus():
-  ksisestus = int(input("Valik: "))
-  while ksisestus >  3 or ksisestus <= 0 :
-    print("Sellist valikut pole")
-    ksisestus = int(input("Proovi midagi muud: "))
-  else:
-    return ksisestus
-
-#Funktsioon saab kasutaja vastuse
-def lahendus(tehe):
-  print('Sisesta vastus: ')
-  print(tehe, end = '')
-  tulemus  = int(input('='))
-  return tulemus
-
-#Funktsioon kontrollib kasutaja vastust
-def xkontroll(KKlahendus, olahendus, k):
-  if KKlahendus == olahendus:
-    k += 1
-    print("Õige")
-    return k
-  else:
-    print("Väär")
-    return k
-
-#Funktsioon teeb suvaliste arvudega tehteid
-def xvalik(arv, k):
-  arv1 = random.randrange(1, 10)
-  arv2 = random.randrange(1, 10)
-
-  if arv is 1:
-    #Genereerib liitmis tehte
-    tehe  = str(arv1) + ' + ' + str(arv2)
-    vastus = arv1 + arv2
-    klahendus = lahendus(tehe)
-    k = xkontroll(klahendus, vastus, k)
-    return k
-  elif arv is 2:
-    #Genereerib lahutamis tehte
-    tehe = str(arv1) + ' - ' + str(arv2)
-    vastus = arv1 - arv2
-    klahendus = lahendus(tehe)
-    k = xkontroll(klahendus, vastus, k)
-    return k
-
-#Funktsioon kuvab tehete koguarvu ja õigete vastuste arvu
-def tulemus(kokku, oige):
-  
-  if kokku > 0:
-    tulemus = oige / kokku
-    protsent = round((tulemus * 100), 2)
-  if kokku == 0:
-    protsent = 0
-  print(f'Sa vastasid {kokku}-st tehtest {oige} õiget vastust')
-  print(f'Sinu tulemuse protsent on: {protsent}%', sep = '')
-  
-def a():
-  algus()
-  tehted()
-
-  o = xsisestus()
-  kokku = 0
-  oige = 0
-  while o != 3:
-    kokku += 1
-    oige = xvalik(o, oige)
-    o = xsisestus()
-  
-  print("Lõpp")
-  tulemus(kokku, oige)
-
-a()
+#Kuvab õigete vastuste protsendi ning ütleb kas on vaja veel harjutada
+    protsent = oiged / korrad * 100
+    print(protsent)
+    if protsent > 61:
+        print("Tubli")
+    else:
+        print("Õpi veel")
+#Annab valida kas alustada algusest
+    kysimus = input("Tahad veel mängida? j või e: " )
+    if kysimus =="e":
+        game = 0
+print("Mäng läbi")
