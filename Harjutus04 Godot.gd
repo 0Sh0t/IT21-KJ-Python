6f8e8ad50c2c1480a86a8cbe0565023752d50a57
KevJn <94998639+0Sh0t@users.noreply.github.com>
2022-04-14 11:44:05 +0300

    Add files via upload

---
+++ Harjutus04 Godot.gd
@@ -0,0 +1,9 @@
+extends Node
+
+func _ready():
+#Massiivi loomine
+	var mangijad = []
+	var nimed = ["Feake","Bradwell","Dreger","Bloggett","Lambole","Daish","Lippiett","Blackie","Stollenbeck","Houseago","Dugall","Sprowson","Kitley","Mcenamin","Allchin","Doghartie","Brierly","Pirrone","Fairnie","Seal","Scoffins","Galer","Matevosian","DeBlase","Cubbin","Izzett","Ebi","Clohisey","Prater","Probart","Samwaye","Concannon","MacLure","Eliet","Kundt","Reyes"]
+#Esimene nimi massiivist
+	print (nimed[0])
+
