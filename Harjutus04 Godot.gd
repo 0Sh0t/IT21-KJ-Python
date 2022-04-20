7da185f70667f88736fba93c6ae30c7a7db81fed
KevJn <94998639+0Sh0t@users.noreply.github.com>
2022-04-20 13:29:46 +0300

    Add files via upload

--- Harjutus04 Godot.gd
+++ Harjutus04 Godot.gd
@@ -1,3 +1,7 @@
+#Kevin Joarand
+#Harjutus 04
+#20.04.2022
+
 extends Node
 
 func _ready():
@@ -5,5 +9,5 @@ func _ready():
 	var mangijad = []
 	var nimed = ["Feake","Bradwell","Dreger","Bloggett","Lambole","Daish","Lippiett","Blackie","Stollenbeck","Houseago","Dugall","Sprowson","Kitley","Mcenamin","Allchin","Doghartie","Brierly","Pirrone","Fairnie","Seal","Scoffins","Galer","Matevosian","DeBlase","Cubbin","Izzett","Ebi","Clohisey","Prater","Probart","Samwaye","Concannon","MacLure","Eliet","Kundt","Reyes"]
 #Esimene nimi massiivist
-	print (nimed[0])
-
+	for nimi in nimed:
+		print (nimi[0])
