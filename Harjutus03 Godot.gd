8c6bec43d22c965e6903eaa2e1f4b142257cb44d
KevJn <94998639+0Sh0t@users.noreply.github.com>
2022-04-20 13:29:31 +0300

    Add files via upload

--- Harjutus03 Godot.gd
+++ Harjutus03 Godot.gd
@@ -1,4 +1,4 @@
-# Harjutus 3
+# Harjutus 03
 # Kevin Joarand
 # 14.04.2022
 
@@ -8,8 +8,8 @@ func _ready():
 	var elud1 = 100
 	var elud2 = 100
 	for x in range(8,15):
-		elud1 -= x
-		print("P1 Life: ", elud1," Hit: ", x)
-	for y in range(8,15):
-		elud2 -= y
-		print("P2 Life: ", elud2," Hit: ", y)
+		for y in range(5,10):
+			elud1 -= x
+			print("P1 Life: ", elud1," Hit: ", x)
+			elud2 -= y
+			print("P2 Life: ", elud2," Hit: ", y)
