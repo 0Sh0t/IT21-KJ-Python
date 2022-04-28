cfef99fb08315d63f3be3ceb7890fc3bd224e613
KevJn <94998639+0Sh0t@users.noreply.github.com>
2022-04-28 13:24:17 +0300

    Add files via upload

--- Harjutus04 Godot.gd
+++ Harjutus04 Godot.gd
@@ -4,13 +4,11 @@
 
 extends Node
 
-var nimed = []
 var mangijad = ["Feake","Bradwell","Dreger","Bloggett","Lambole","Daish","Lippiett","Blackie","Stollenbeck","Houseago","Dugall","Sprowson","Kitley","Mcenamin","Allchin","Doghartie","Brierly","Pirrone","Fairnie","Seal","Scoffins","Galer","Matevosian","DeBlase","Cubbin","Izzett","Ebi","Clohisey","Prater","Probart","Samwaye","Concannon","MacLure","Eliet","Kundt","Reyes"]
 
 func _ready(): 
 	print("Mängijate arv: ", len(mangijad))
 	print("Esimene nimi: ", mangijad[0])
-	print(nimed)
 	mangijad.erase("Reyes")
 	mangijad.append("Kevin")
 	print(mangijad.max())
---
+++ Harjutus04.2 Godot.gd
@@ -0,0 +1,14 @@
+#Kevin Joarand
+#Harjutus 04
+#20.04.2022
+
+extends Node
+
+func _ready():
+	var player = {"name":"Vello", "posx":69, "posy":420, "health":100, "items":["Mõõk",""], "kuld":0}
+	
+	print(player.kuld)
+	for i in range(5):
+		player.kuld += 5
+		print("+5 Kulda")
+	print(player.kuld)
