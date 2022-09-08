92222bde9cd5cf739ed0e407b86ba2f282f1ce7b
KevJn <94998639+0Sh0t@users.noreply.github.com>
2022-09-08 11:39:07 +0300

    Add files via upload

--- JavaH2.java
+++ JavaH2.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import javax.swing.SpringLayout;
+
 //1. Teisendab tollid meetriteks
 public class JavaH2 {
 
@@ -7,7 +9,10 @@ public class JavaH2 {
     teisendamine();
     ellips();
     aeg();
-    sportlane();
+    float sportlane1 = sportlane();
+    float sportlane2 = sportlane();
+    System.out.println(sportlane2);
+    
     }
 
     public static void teisendamine(){
@@ -33,14 +38,18 @@ public class JavaH2 {
     }
     
     //4. Leiab sportlase kiiruse
-    public static void sportlane(){
+    public static float sportlane(){
         Scanner in = new Scanner(System.in);
         System.out.print("Sisesta aeg: ");
         int aeg = in.nextInt();
         System.out.print("Sisesta distants: ");
         int distants = in.nextInt();
-        float kiirus = (float)distants / aeg;
-        System.out.println("Kiirus on: " + kiirus);
+        float kiirus1 = (float)distants / aeg;
+        return kiirus1;
+    }
+    public static float vahe(float sportlane1, sportlane2){
+
+
     }
     
 }
