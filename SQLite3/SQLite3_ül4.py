b747d65e867ea294fdcc3f7b208aca8c6d2006d8
KevJn <94998639+0Sh0t@users.noreply.github.com>
2023-05-04 15:42:29 +0300

    Add files via upload

--- SQLite3/SQLite3_ül4.py
+++ SQLite3/SQLite3_ül4.py
@@ -1,10 +1,12 @@
 import sqlite3
 import tkinter as tk
 from tkinter import ttk
+from ttkbootstrap import Style
 
 # Create a connection to the database
 conn = sqlite3.connect('C:/sqlite/epood_kjoarand.db')
 c = conn.cursor()
+
 
 # Define a function to retrieve data from the table and display it in the treeview widget
 def display_data():
@@ -19,15 +21,19 @@ def display_data():
     for row in data:
         treeview.insert("", "end", values=row)
 
+
 # Define a function to update the current page number and display the data on the new page
 def update_page(*args):
     global current_page
     current_page = int(scrollbar.get())
     display_data()
 
+
 # Create the main window
 root = tk.Tk()
+style = Style(theme='darkly')
 root.title("Data Viewer")
+
 
 # Create a treeview widget to display the data
 treeview = ttk.Treeview(root, columns=("First Name", "Last Name", "Email", "Car Make", "Car Model", "Car Year", "Car Price"))
@@ -37,21 +43,25 @@ treeview.heading("Last Name", text="Last Name")
 treeview.heading("Email", text="Email")
 treeview.heading("Car Make", text="Car Make")
 treeview.heading("Car Model", text="Car Model")
-treeview.heading("Car Year", text="Car Year")
+treeview.heading("Car Year", text="Car Year",)
 treeview.heading("Car Price", text="Car Price")
-treeview.pack()
+treeview.pack(expand=True, fill="both")
+
 
 # Add a frame to contain the scrollbar and the page size selector
 frame = tk.Frame(root)
 frame.pack(side="bottom")
 
+
 # Add a scrollbar to navigate between pages
 scrollbar = ttk.Scrollbar(frame, orient="horizontal", command=update_page)
-scrollbar.pack(side="bottom", fill="x")
+scrollbar.pack(side="bottom", fill="both")
+
 
 # Add a label to display the page size selector
 label = tk.Label(frame, text="Rows per page:")
 label.pack(side="left")
+
 
 # Add a combobox to select the number of rows to display per page
 page_size_var = tk.StringVar()
@@ -60,18 +70,23 @@ page_size_choices = ["5", "10", "25", "50", "100"]
 page_size_combobox = ttk.Combobox(frame, textvariable=page_size_var, values=page_size_choices, state="readonly")
 page_size_combobox.pack(side="left")
 
+
 # Bind the combobox to the display_data() function so that the data is updated when the page size changes
 page_size_combobox.bind("<<ComboboxSelected>>", lambda e: display_data())
+
 
 # Initialize global variables for the current page and page size
 current_page = 1
 page_size = int(page_size_var.get())
 
+
 # Call the display_data() function to display the initial data
 display_data()
 
+
 # Start the main event loop
 root.mainloop()
+
 
 # Close the connection to the database
 conn.close()
