259b1bfa1b33eb71d3b26237ce85d7faa28ab4d8
KevJn <94998639+0Sh0t@users.noreply.github.com>
2023-05-16 15:43:35 +0300

    Add files via upload

--- SQLite3/SQLite3_ül4.py
+++ SQLite3/SQLite3_ül4.py
@@ -1,92 +1,177 @@
 import sqlite3
-import tkinter as tk
-from tkinter import ttk
+from tkinter import Tk, Listbox, Scrollbar, Frame, Button, Entry, messagebox, Label, END
 from ttkbootstrap import Style
 
-# Create a connection to the database
-conn = sqlite3.connect('C:/sqlite/epood_kjoarand.db')
-c = conn.cursor()
+# Andmebaasi teekond ja tabel
+database_path = "C:/sqlite/epood_kjoarand.db"
+table_name = "kjoarand"
 
+# Loome andmebaasi ühenduse
+conn = sqlite3.connect(database_path)
+cursor = conn.cursor()
 
-# Define a function to retrieve data from the table and display it in the treeview widget
-def display_data():
-    # Clear the treeview widget
-    for i in treeview.get_children():
-        treeview.delete(i)
-    # Retrieve the selected number of rows from the table
-    offset = page_size * (current_page - 1)
-    c.execute(f"SELECT * FROM kjoarand LIMIT {page_size} OFFSET {offset}")
-    data = c.fetchall()
-    # Insert the data into the treeview widget
+# Funktsioon, mis tagastab lehekülje andmed
+def get_page_data(page_num, page_size):
+    offset = (page_num - 1) * page_size
+    query = f"SELECT * FROM {table_name} LIMIT {page_size} OFFSET {offset}"
+    cursor.execute(query)
+    return cursor.fetchall()
+
+# Funktsioon, mis arvutab lehekülgede arvu
+def calculate_page_count(page_size):
+    query = f"SELECT COUNT(*) FROM {table_name}"
+    cursor.execute(query)
+    total_records = cursor.fetchone()[0]
+    return (total_records // page_size) + 1
+
+# Funktsioon, mis kuvab lehekülje andmed
+def display_page(page_num):
+    listbox.delete(0, END)
+    page_size = 5  # Muuda vastavalt soovile
+    data = get_page_data(page_num, page_size)
     for row in data:
-        treeview.insert("", "end", values=row)
+        for i, value in enumerate(row):
+            listbox.insert("end", f"{columns[i]}: {value}")
+        listbox.insert("end", "------------------------")
 
-
-# Define a function to update the current page number and display the data on the new page
-def update_page(*args):
+# Funktsioon, mis kerib edasi
+def next_page():
     global current_page
-    current_page = int(scrollbar.get())
-    display_data()
+    if current_page < page_count:
+        current_page += 1
+        display_page(current_page)
 
+# Funktsioon, mis kerib tagasi
+def previous_page():
+    global current_page
+    if current_page > 1:
+        current_page -= 1
+        display_page(current_page)
 
-# Create the main window
-root = tk.Tk()
-style = Style(theme='darkly')
-root.title("Data Viewer")
+# Funktsioon, mis otsib andmeid
+def search_data():
+    search_term = search_entry.get()
+    query = f"SELECT * FROM {table_name} WHERE 'First Name' LIKE ? OR 'Last Name' LIKE ? OR Email LIKE ? OR 'Car Make' LIKE ? OR 'Car Model' LIKE ? OR 'Car Year' LIKE ? OR 'Car Price' LIKE ?"
+    cursor.execute(query, (f"%{search_term}%", f"%{search_term}%", f"%{search_term}%", f"%{search_term}%", f"%{search_term}%", f"%{search_term}%", f"%{search_term}%"))
+    search_results = cursor.fetchall()
+    if not search_results:
+        messagebox.showinfo("Otsingu tulemused", "Andmeid ei leitud vastavalt otsingukriteeriumidele.")
+    else:
+        listbox.delete(0, END)
+        for row in search_results:
+            for i, value in enumerate(row):
+                listbox.insert("end", f"{columns[i]}: {value}")
+            listbox.insert("end", "------------------------")
 
+# Funktsioon, mis lisab andmeid
+def add_data():
+    values = [entry.get() for entry in add_entries]
+    if any(value == "" for value in values):
+        messagebox.showwarning("Andmete lisamine", "Palun täitke kõik väljad.")
+        return
+        query = f"INSERT INTO {table_name} ('First Name', 'Last Name', Email, 'Car Make', 'Car Model', 'Car Year', 'Car Price') VALUES (?, ?, ?, ?, ?, ?, ?)"
+    cursor.execute(query, values)
+    conn.commit()
+    messagebox.showinfo("Andmete lisamine", "Andmed on edukalt lisatud.")
+    # Clear the entry fields
+    for entry in add_entries:
+        entry.delete(0, END)
+    # Refresh the current page to display the updated data
+    display_page(current_page)
 
-# Create a treeview widget to display the data
-treeview = ttk.Treeview(root, columns=("First Name", "Last Name", "Email", "Car Make", "Car Model", "Car Year", "Car Price"))
-treeview.heading("#0", text="ID")
-treeview.heading("First Name", text="First Name")
-treeview.heading("Last Name", text="Last Name")
-treeview.heading("Email", text="Email")
-treeview.heading("Car Make", text="Car Make")
-treeview.heading("Car Model", text="Car Model")
-treeview.heading("Car Year", text="Car Year",)
-treeview.heading("Car Price", text="Car Price")
-treeview.pack(expand=True, fill="both")
+# Funktsioon, mis kustutab valitud rea
+def delete_row():
+    selected_indices = listbox.curselection()
+    if not selected_indices:
+        messagebox.showwarning("Rea kustutamine", "Palun valige rida, mida soovite kustutada.")
+        return
+    result = messagebox.askyesno("Rea kustutamine", "Kas olete kindel, et soovite valitud rea kustutada?")
+    if result == "yes":
+        for index in reversed(selected_indices):
+            data = get_page_data(current_page, 5)  # Assuming page size is 5
+            row_index = index % len(data)
+            record_id = data[row_index][0]  # Assuming ID is in the first column
+            query = f"DELETE FROM {table_name} WHERE ID = ?"
+            cursor.execute(query, (record_id,))
+            conn.commit()
+        display_page(current_page)
+        messagebox.showinfo("Rea kustutamine", "Valitud read on edukalt kustutatud.")
 
+# Loome tkinteri akna
+root = Tk()
+root.title("Andmete kuvamine")
+root.geometry("500x400")
 
-# Add a frame to contain the scrollbar and the page size selector
-frame = tk.Frame(root)
-frame.pack(side="bottom")
+# Lae ttkbootstrap stiil
+style = Style(theme="darkly")
 
+# Loome keritava raami
+frame = Frame(root)
+frame.pack(fill="both", expand=True)
 
-# Add a scrollbar to navigate between pages
-scrollbar = ttk.Scrollbar(frame, orient="horizontal", command=update_page)
-scrollbar.pack(side="bottom", fill="both")
+# Loome keritava listboxi
+listbox = Listbox(frame)
+listbox.pack(side="left", fill="both", expand=True)
 
+# Loome kerimisribad
+yscrollbar = Scrollbar(frame, orient="vertical")
+yscrollbar.pack(side="right", fill="y")
+listbox.config(yscrollcommand=yscrollbar.set)
+yscrollbar.config(command=listbox.yview)
 
-# Add a label to display the page size selector
-label = tk.Label(frame, text="Rows per page:")
-label.pack(side="left")
+# Nupud lehekülje kerimiseks
+button_frame = Frame(root)
+button_frame.pack()
 
+previous_button = Button(button_frame, text="Eelmine leht", command=previous_page)
+previous_button.pack(side="left")
 
-# Add a combobox to select the number of rows to display per page
-page_size_var = tk.StringVar()
-page_size_var.set("10")
-page_size_choices = ["5", "10", "25", "50", "100"]
-page_size_combobox = ttk.Combobox(frame, textvariable=page_size_var, values=page_size_choices, state="readonly")
-page_size_combobox.pack(side="left")
+next_button = Button(button_frame, text="Järgmine leht", command=next_page)
+next_button.pack(side="left")
 
+# Otsinguväljad
+search_label = Label(root, text="Otsi:")
+search_label.pack()
 
-# Bind the combobox to the display_data() function so that the data is updated when the page size changes
-page_size_combobox.bind("<<ComboboxSelected>>", lambda e: display_data())
+search_entry = Entry(root)
+search_entry.pack()
 
+search_button = Button(root, text="Otsi", command=search_data)
+search_button.pack()
 
-# Initialize global variables for the current page and page size
+# Andmete lisamise väljad
+add_label = Label(root, text="Lisa andmed:")
+add_label.pack()
+
+add_entries = []
+add_fields = ["First Name", "Last Name", "Email", "Car Make", "Car Model", "Car Year", "Car Price"]
+
+for field in add_fields:
+    add_label = Label(root, text=field + ":")
+    add_label.pack()
+
+    add_entry = Entry(root)
+    add_entry.pack()
+
+    add_entries.append(add_entry)
+
+add_button = Button(root, text="Lisa", command=add_data)
+add_button.pack()
+
+# Kustutamise nupp
+delete_button = Button(root, text="Kustuta rida", command=delete_row)
+delete_button.pack()
+
+# Kuvame esimese lehekülje andmed
 current_page = 1
-page_size = int(page_size_var.get())
+columns = ["ID", "First Name", "Last Name", "Email", "Car Make", "Car Model", "Car Year", "Car Price"]
 
+page_count = calculate_page_count(5)  # Muuda vastavalt soovile
+display_page(current_page)
 
-# Call the display_data() function to display the initial data
-display_data()
-
-
-# Start the main event loop
+# Käivitame ttkbootstrap stiiliga tkinter
 root.mainloop()
 
-
-# Close the connection to the database
+# Sulgeme andmebaasi ühenduse
+cursor.close()
 conn.close()
